7eaba6daed6f5ac056b3f895feafe4588bc03c1d
EstebanBedoya <estebegi@gmail.com>
2019-11-05 17:39:31 -0500

    travis

--- S3/pages/Deudas/index.vue
+++ S3/pages/Deudas/index.vue
@@ -1,49 +1,62 @@
 <template>
   <div class="mt-4" id="app">
-    
-    <b-row class="mt-3">
-      <b-col cols="10" md="auto" v-for="(deuda,index) in cards" :key="index">
-        <b-card
-          :header="deuda.entidad"
-          border-variant="info"
-          header-bg-variant="info"
-          header-text-variant="white"
-        >
-          <b-card-text>Monto total: {{deuda.montoTotal}}</b-card-text>
-          <b-card-text>Cuota: {{deuda.cuota}}</b-card-text>
-          <b-card-text>Proximo pago: {{deuda.fechaPago}}</b-card-text>
-
-          
-          <b-badge
-            href="#"
-            variant="danger"
-            v-b-modal.modal-2
-            @click="datosParaEliminarDeuda(index, deuda._id)"
-            v-b-popover.hover.top="'Eliminar Deuda'"
+    <b-card header="Deudas" header-bg-variant="info" header-text-variant="white">
+      <template v-slot:header>
+        Deudas
+        <b-button  style="float:right"  :to="'/deudas/registrarDeuda'" variant="primary">
+          <i class="material-icons">add</i>
+        </b-button>
+      </template>
+      <b-row class="mt-3">
+        <b-col cols="10" md="auto" v-for="(deuda,index) in cards" :key="index">
+          <b-card
+            :header="deuda.entidad"
+            border-variant="info"
+            header-bg-variant="info"
+            header-text-variant="white"
           >
-            <i class="material-icons">delete</i>
-          </b-badge>
-          <b-badge href="#" variant="primary" v-b-modal.modal-1 @click="setIdDeuda(deuda._id)"
-          v-b-popover.hover.top="'Asignar tarjeta'">
-            <i class="material-icons">credit_card</i>
-          </b-badge>
-          
-        </b-card>
-      </b-col>
+            <b-card-text>Monto total: {{deuda.montoTotal}}</b-card-text>
+            <b-card-text>Cuota: {{deuda.cuota}}</b-card-text>
+            <b-card-text>Proximo pago: {{deuda.fechaPago}}</b-card-text>
 
-      <b-col cols="10" md="auto">
+            <b-badge
+              href="#"
+              variant="danger"
+              v-b-modal.modal-2
+              @click="datosParaEliminarDeuda(index, deuda._id)"
+              v-b-popover.hover.top="'Eliminar Deuda'"
+            >
+              <i class="material-icons">delete</i>
+            </b-badge>
+            <b-badge
+              href="#"
+              variant="primary"
+              v-b-modal.modal-1
+              @click="setIdDeuda(deuda._id)"
+              v-b-popover.hover.top="'Asignar tarjeta'"
+            >
+              <i class="material-icons">credit_card</i>
+            </b-badge>
+          </b-card>
+        </b-col>
+
+        <!-- <b-col cols="10" lg="3">
         <b-card
+          header="agregar deuda"
+          footer
           border-variant="info"
           header-bg-variant="info"
           header-text-variant="white"
-          style="height: 233px;"
         >
-          <b-button :to="'/deudas/registrarDeuda'" variant="outline-primary">Agegar Deuda</b-button>
+          <b-button :to="'/deudas/registrarDeuda'" variant="primary">
+            <i class="material-icons">add</i>
+          </b-button>
         </b-card>
-      </b-col>
+        </b-col>-->
 
-      <!-- <b-card header="More" border-variant="info" header-bg-variant="info" header-text-variant="white"></b-card> -->
-    </b-row>
+        <!-- <b-card header="More" border-variant="info" header-bg-variant="info" header-text-variant="white"></b-card> -->
+      </b-row>
+    </b-card>
     <b-modal id="modal-1" ref="my-modal" hide-footer title="APO">
       <div class="d-block text-center">
         <h3>Seleccionar tarjeta</h3>
@@ -89,7 +102,6 @@ export default {
   },
   methods: {
     deleteCard(index, id) {
-
       this.$axios
         .delete(`http://localhost:81/deudas/${id}`, id)
         .then(response => {
@@ -101,7 +113,6 @@ export default {
       this.cards.splice(index, 1);
     },
     getAllRecords() {
-
       this.$axios.get("http://localhost:81/deudas/").then(response => {
         // console.log(response.data.deudas);
 
@@ -114,7 +125,6 @@ export default {
       });
     },
     getAllTarjetas() {
-
       this.$axios.get(`http://localhost:81/tarjetas/`).then(response => {
         if (response.data.error) {
           this.errorMessage = reponse.data.message;
@@ -125,9 +135,11 @@ export default {
       });
     },
     addTarjetaInDeuda(id, value) {
-      this.$axios.put(`http://localhost:81/deudas/${id}/${value}`, id, value).then(response => {
-        console.log(response);
-      });
+      this.$axios
+        .put(`http://localhost:81/deudas/${id}/${value}`, id, value)
+        .then(response => {
+          console.log(response);
+        });
     },
     setIdDeuda(id) {
       this.idDeuda = id;
